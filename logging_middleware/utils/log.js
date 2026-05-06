a2f58d219b9b890d9cd3c47b4cd492c47c5acf0d
tanspan <tanusrikaranam@gmail.com>
2026-05-06 15:50:48 +0530

    c2-vehicle maintenance scd

--- logging_middleware/utils/log.js
+++ logging_middleware/utils/log.js
@@ -12,7 +12,7 @@ const Log = async (stack, level, packageName, message) => {
       },
       {
         headers: {
-          Authorization: `Bearer YOUR_ACCESS_TOKEN`,
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJhdi5zYy51NGFpZTIzMDQ0QGF2LnN0dWRlbnRzLmFtcml0YS5lZHUiLCJleHAiOjE3NzgwNjAxMjUsImlhdCI6MTc3ODA1OTIyNSwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6IjgxNTc0NzFlLTM2YTItNDJlZS1iYTI5LTQxNjk1NDgwY2VhNiIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6InMuay50YW51c3JpIiwic3ViIjoiYjQ5YjY5YjUtOTlkMC00ZjkyLTkwNjgtNWIyZDgyMWUwMzE1In0sImVtYWlsIjoiYXYuc2MudTRhaWUyMzA0NEBhdi5zdHVkZW50cy5hbXJpdGEuZWR1IiwibmFtZSI6InMuay50YW51c3JpIiwicm9sbE5vIjoiYXYuc2MudTRhaWUyMzA0NCIsImFjY2Vzc0NvZGUiOiJQVEJNbVEiLCJjbGllbnRJRCI6ImI0OWI2OWI1LTk5ZDAtNGY5Mi05MDY4LTViMmQ4MjFlMDMxNSIsImNsaWVudFNlY3JldCI6IldrS3FCSGFHdkZYdkJ6Q0MifQ.GAxdu65t1bPSsKC8zy6YV-X-6VOOSPd3gnBTClI5I1Y`,
         },
       }
     );
