f6d42de04c037074831f54e6e21bd946b1a32403
tanspan <tanusrikaranam@gmail.com>
2026-05-06 14:19:57 +0530

    pre setup

---
+++ logging_middleware/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const Log = require("./utils/log");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+
+  await Log(
+    "backend",
+    "info",
+    "route",
+    "Root API accessed"
+  );
+
+  res.json({
+    success: true,
+    message: "Backend Running Successfully",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
